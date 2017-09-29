1537cead0ce259f6654c8181a432d9e55126ffa4
Alessandro Manfredi <alle.manfredi@gmail.com>
2017-09-29 15:41:08 +0200

    Update InitializeDatabase.java

--- PC-SIDE/src/InitializeDatabase.java
+++ PC-SIDE/src/InitializeDatabase.java
@@ -17,51 +17,59 @@ public class InitializeDatabase implements Runnable{
 	public void run() {
 		
 		try{
-			  Class.forName("com.mysql.jdbc.Driver");
+			
+			Class.forName("com.mysql.jdbc.Driver");
 
-		      System.out.println("Connecting to database...");
-		      TelemetryData.getInstance().listLog.addElement("Connecting to database...");
-		      
-		      TelemetryData.getInstance().conn = DriverManager.getConnection(TelemetryData.getInstance().DB_URL,TelemetryData.getInstance().USER,TelemetryData.getInstance().PASS);
+			System.out.println("Connecting to database...");
+			TelemetryData.getInstance().listLog.addElement("Connecting to database...");
 
-		      System.out.println("Creating statement...");
-		      TelemetryData.getInstance().listLog.addElement("Creating statement...");
-		      
-		      TelemetryData.getInstance().stmt = TelemetryData.getInstance().conn.createStatement();
-		      
-		      
-		      System.out.println("Database created successfully...");
-		      TelemetryData.getInstance().listLog.addElement("Database created successfully...");
-		      
-		      
-		      this.InitQuery();
+			TelemetryData.getInstance().conn = DriverManager.getConnection(TelemetryData.getInstance().DB_URL,TelemetryData.getInstance().USER,TelemetryData.getInstance().PASS);
+
+			System.out.println("Creating statement...");
+			TelemetryData.getInstance().listLog.addElement("Creating statement...");
+
+			TelemetryData.getInstance().stmt = TelemetryData.getInstance().conn.createStatement();
+
+			System.out.println("Database created successfully...");
+			TelemetryData.getInstance().listLog.addElement("Database created successfully...");
+
+
+			this.InitQuery();
 		      
 		      
 		   }catch(SQLException se){
 		      //Handle errors for JDBC
 			   
 			   TelemetryData.getInstance().listLog.addElement(se.toString());
-		      se.printStackTrace();
+		      	   se.printStackTrace();
+			
 		   }catch(Exception e){
 		      //Handle errors for Class.forName
 			   
 			   TelemetryData.getInstance().listLog.addElement(e.getMessage());
-		      e.printStackTrace();
+		     	   e.printStackTrace();
+			
 		   }finally{
 			   
-		      //finally block used to close resources
 		      try{
 		         if(TelemetryData.getInstance().stmt!=null)
+				 
 		            ((Connection) TelemetryData.getInstance().stmt).close();
+			      
 		      }catch(SQLException se2){
+			      
 		    	  TelemetryData.getInstance().listLog.addElement(se2.getMessage());
-		      }// nothing we can do
+		      }
+			
 		      try{
 		         if(TelemetryData.getInstance().conn!=null)
+				 
 		        	 TelemetryData.getInstance().conn.close();
 		      }catch(SQLException se){
+			      
 		         se.printStackTrace();
 		         TelemetryData.getInstance().listLog.addElement(se.getMessage());
+			      
 		      }//end finally try
 		   }//end try
 		
@@ -69,22 +77,17 @@ public class InitializeDatabase implements Runnable{
 	
 	private void InitQuery() throws SQLException{
 		
-		PreparedStatement query = TelemetryData.getInstance().conn .prepareStatement("INSERT INTO Sessione ( Nome , Data  ) VALUES ( ? , ?)",PreparedStatement.RETURN_GENERATED_KEYS);
+	    PreparedStatement query = TelemetryData.getInstance().conn .prepareStatement("INSERT INTO Sessione ( Nome , Data  ) VALUES ( ? , ?)",PreparedStatement.RETURN_GENERATED_KEYS);
 	    query.setString(1, TelemetryData.getInstance().nomeSessione);
 	    query.setDate(2, new java.sql.Date(date.getTime()));
 	    query.execute();
 	    
 	    ResultSet rs = query.getGeneratedKeys();
-	    if(rs.next())
-        {
+		
+	    if(rs.next()){
 	    	TelemetryData.getInstance().idSessione = rs.getInt(1);
-        }
-        
-        
-        System.out.println("IDDDDD " + TelemetryData.getInstance().idSessione);
-
+            }
+   
+       	    System.out.println("IDDDDD " + TelemetryData.getInstance().idSessione);
 	}
-	
-	
-
 }
